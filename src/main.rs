c01df7d180252ddb323c2b5b49b2a1fb132faaac
Simeon Widdis <sawiddis@amazon.com>
2024-12-10 13:40:54 -0800

    Add optional ability to use GH auth token
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
+// Some error variants (in particular those from Octocrab) are large, which causes a performance hit
+// while calling fallible methods. Since the application isn't performance-critical and the
+// workaround is annoying, we silence the associated lint project-wide.
+#![allow(clippy::result_large_err)]
+
+use octocrab::Octocrab;
+use std::env;
 use thiserror::Error;
 
-/// Metric collection error represents the top-level error type of this metric collector. This holds
-/// the full set of errors that the application may exit with.
+/// MetricCollectionError represents the top-level error type of the application. This holds the
+/// full set of errors that the application may exit with.
 #[derive(Error, Debug)]
 pub enum MetricCollectionError {
-    #[error("failed to interact with the github API")]
+    #[error("failed to interact with the GitHub API")]
     GithubError(#[from] octocrab::Error),
+}
+
+fn octocrab_client() -> Result<Octocrab, octocrab::Error> {
+    let token = env::var("GH_TOKEN").unwrap_or("".into());
+    let mut builder = octocrab::OctocrabBuilder::new();
+    if !token.is_empty() {
+        builder = builder.personal_token(token);
+    };
+    builder.build()
 }
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), MetricCollectionError> {
-    let octocrab = octocrab::instance();
-    let page = octocrab.issues("opensearch-project", "OpenSearch-Dashboards")
+    let octocrab = octocrab_client()?;
+    let page = octocrab
+        .issues("opensearch-project", "OpenSearch-Dashboards")
         .list()
         .per_page(50)
         .send()
