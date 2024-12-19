79a85dcf13fa80989e655d08c6b3792683c38f7a
Simeon Widdis <sawiddis@amazon.com>
2024-12-19 15:08:30 -0800

    Add basic backfill logic for issues

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use octocrab::{models::issues::Issue, Octocrab, Page};
+use chrono::{DateTime, Utc};
+use octocrab::{models::issues::Issue, params::issues::Sort, Octocrab, Page};
 use ratelimit::Ratelimiter;
-use std::{env, sync::Arc, time::{Duration, Instant}};
+use std::{env, time::Duration};
 use thiserror::Error;
-use tokio::{sync::{mpsc::{self, Receiver}, oneshot}, task::JoinError};
+use tokio::sync::{mpsc, oneshot};
 
 /// MetricCollectionError represents the top-level error type of the application, with all its
 /// failure modes
@@ -22,15 +23,14 @@ enum EntityType {
 /// Update messages for GitHub entities
 #[derive(Debug, Clone)]
 struct EntityUpdate {
-    e_type: EntityType,
-    id: u64,
+    _e_type: EntityType,
+    _id: u64,
 }
 
 /// Types of entities we care about requesting from the GitHub API
 #[derive(Debug)]
 enum DataRequest {
-    IssuePage(usize, oneshot::Sender<Page<Issue>>),
-    Issue(usize, oneshot::Sender<Issue>),
+    IssuePage(u32, oneshot::Sender<Page<Issue>>),
 }
 
 async fn github_ratelimiter(client: &Octocrab) -> Ratelimiter {
@@ -43,11 +43,10 @@ async fn github_ratelimiter(client: &Octocrab) -> Ratelimiter {
                 .initial_available(limits.rate.remaining as u64)
                 .build()
                 .expect("GitHub returned an invalid rate limit state: {limits:?}")
-        },
+        }
         Err(err) => {
-            log::warn!("unable to fetch rate limit information from GitHub: {err}");
-            // If we aren't able to fetch the rate limit, just guess based on the PAT rate limit of
-            // 5000 requests/hr, but start with 0 tokens.
+            log::warn!("unable to fetch rate limit information from GitHub: \"{err}\", using default options");
+            // Guess the PAT default rate limit (5000/hr), but start with 0 tokens
             Ratelimiter::builder(1, Duration::from_millis(720))
                 .max_tokens(5000)
                 .build()
@@ -74,15 +73,37 @@ fn octocrab_client() -> Result<Octocrab, MetricCollectionError> {
     }
 }
 
-fn handle_data_request(req: DataRequest) {
-    log::debug!("Received request: {req:?}")
+async fn get_issue_page(client: &Octocrab, page: u32) -> Page<Issue> {
+    let page = client
+        .issues("opensearch-project", "opensearch-dashboards")
+        .list()
+        .sort(Sort::Created)
+        .per_page(100)
+        .page(page)
+        .send()
+        .await;
+    page.unwrap() // TODO handle err
+}
+
+async fn handle_data_request(client: &Octocrab, req: DataRequest) {
+    log::debug!("Received request: {req:?}");
+    match req {
+        DataRequest::IssuePage(page, sender) => {
+            let result = get_issue_page(client, page).await;
+            let _ = sender.send(result);
+        }
+    }
 }
 
 /// Spawn a task to handle all requests to the GitHub API, respecting rate limiting. We keep this as
 /// a dedicated task to respect GitHub's published best practices to avoid parallel requests.
-fn start_request_handling(client: Octocrab, mut requests: Receiver<DataRequest>) -> tokio::task::JoinHandle<()> {
+fn start_request_handling(
+    client: Octocrab,
+    mut requests: mpsc::Receiver<DataRequest>,
+) -> tokio::task::JoinHandle<()> {
     tokio::spawn(async move {
         let limiter = github_ratelimiter(&client).await;
+        log::info!("starting main request loop");
 
         loop {
             if let Err(sleep) = limiter.try_wait() {
@@ -90,7 +111,7 @@ fn start_request_handling(client: Octocrab, mut requests: Receiver<DataRequest>)
             }
 
             match requests.recv().await {
-                Some(req) => handle_data_request(req),
+                Some(req) => handle_data_request(&client, req).await,
                 None => break,
             }
         }
@@ -99,22 +120,57 @@ fn start_request_handling(client: Octocrab, mut requests: Receiver<DataRequest>)
 
 /// Spawn a task to keep track of the newest events in GH, and update the corresponding database
 /// records. This is the bulk of the long-term scraping and should typically never halt.
-fn start_update_tracking(requests: mpsc::Sender<DataRequest>, updates: mpsc::Sender<EntityUpdate>, track_start: &Instant) -> tokio::task::JoinHandle<()> {
+fn start_update_tracking(
+    _requests: mpsc::Sender<DataRequest>,
+    _updates: mpsc::Sender<EntityUpdate>,
+    _track_start: DateTime<Utc>,
+) -> tokio::task::JoinHandle<()> {
     tokio::spawn(async move {
+        log::info!("starting main update loop");
+
         loop {
             // TODO use Events API to detect updates and put in SQLite store
-            updates.send(EntityUpdate { e_type: EntityType::Issue, id: 0 }).await.unwrap();
+            break;
         }
     })
 }
 
 /// Spawn a task to backfill any missing data from before the startup time. Terminates when there's
 /// nothing left to fill.
-fn start_backfill(requests: mpsc::Sender<DataRequest>, updates: mpsc::Sender<EntityUpdate>, ignore_updated_after: &Instant)  -> tokio::task::JoinHandle<()> {
+fn start_backfill(
+    requests: mpsc::Sender<DataRequest>,
+    updates: mpsc::Sender<EntityUpdate>,
+    ignore_updated_after: DateTime<Utc>,
+) -> tokio::task::JoinHandle<()> {
     tokio::spawn(async move {
-        loop {
-            // TODO backfill all the GH data we care about (issues/comments/pulls)
-            updates.send(EntityUpdate { e_type: EntityType::Issue, id: 1 }).await.unwrap();
+        let mut curr_page = 1;
+        let mut has_next = true;
+
+        log::info!("starting main backfill loop");
+
+        while has_next {
+            let (send, rec) = oneshot::channel();
+            // If the request handler stops running and we get errors while communicating with it,
+            // just shutdown this task silently
+            let Ok(_) = requests.send(DataRequest::IssuePage(curr_page, send)).await else {
+                return;
+            };
+            let Ok(page) = rec.await else { return };
+
+            has_next = page.next.is_some();
+            for issue in page
+                .into_iter()
+                .filter(|i| i.updated_at < ignore_updated_after)
+            {
+                updates
+                    .send(EntityUpdate {
+                        _e_type: EntityType::Issue,
+                        _id: *issue.id,
+                    })
+                    .await
+                    .unwrap();
+            }
+            curr_page += 1;
         }
     })
 }
@@ -122,29 +178,39 @@ fn start_backfill(requests: mpsc::Sender<DataRequest>, updates: mpsc::Sender<Ent
 /// Preprocess and upload records found in scrape tasks to OpenSearch.
 fn start_uploading(mut receiver: mpsc::Receiver<EntityUpdate>) -> tokio::task::JoinHandle<()> {
     tokio::spawn(async move {
-        while let Some(message) = receiver.recv().await {
-            log::debug!("{message:?}");
+        log::info!("starting main upload loop");
+
+        while let Some(update) = receiver.recv().await {
+            log::trace!("processing entity update: {update:?}");
         }
     })
 }
 
-#[tokio::main(flavor="current_thread")]
+#[tokio::main]
 async fn main() -> Result<(), MetricCollectionError> {
+    env_logger::init();
+
     let client = octocrab_client()?;
     let (request_sender, request_receiver) = mpsc::channel::<DataRequest>(32);
     let (scrape_sender, scrape_receiver) = mpsc::channel::<EntityUpdate>(32);
-    let now = std::time::Instant::now();
-    
+    let now = Utc::now();
+
+    log::info!("successfully set up GitHub client and channels, spawning main tasks");
+
     let result = tokio::try_join!(
         start_request_handling(client, request_receiver),
-        start_update_tracking(request_sender.clone(), scrape_sender.clone(), &now),
-        start_backfill(request_sender, scrape_sender, &now),
+        start_update_tracking(request_sender.clone(), scrape_sender.clone(), now.clone()),
+        start_backfill(request_sender, scrape_sender, now),
         start_uploading(scrape_receiver),
     );
+
+    log::info!("main task batch has terminated, shutting down");
 
     // In normal operation, the above join never terminates. Handle any errors that came up.
     match result {
         Ok(_) => Ok(()),
-        Err(err) => Err(MetricCollectionError::TaskRuntimeError { message: err.to_string() }),
+        Err(err) => Err(MetricCollectionError::TaskRuntimeError {
+            message: err.to_string(),
+        }),
     }
 }
